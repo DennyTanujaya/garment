d9282d3189f96496c77d4e9bfb4f416e357d1658
Denny Tanujaya <tanujayadenny@yahoo.com>
2020-10-06 13:57:30 +0700

    added first files

---
+++ assets/header.php
@@ -0,0 +1,4 @@
+<title>Aplikasi Inventory</title>
+<link rel="stylesheet" type="text/css" href="halaman_style.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<script type="text/javascript" src="jquery.js"></script>
